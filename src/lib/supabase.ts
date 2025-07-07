5ef3e3064812f5d9892101f97afa7d277c09ab1c
Warren John Caruana <warrencaruana1@gmail.com>
2025-07-08 07:47:16 +0800

    Log vars

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(
+  "process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+console.log(
+  "process.env.SUPABASE_SERVICE_ROLE_KEY",
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
