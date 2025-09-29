ae7d1cf1a933b5688a6f5f9d1549b3e01093d2d0
Warren John Caruana <warrencaruana1@gmail.com>
2025-09-29 20:53:04 +0800

    Handle cors on clock out

--- src/app/api/timelogs/clock-out/route.ts
+++ src/app/api/timelogs/clock-out/route.ts
@@ -1,13 +1,23 @@
+import { handleCors } from '@/lib/cors';
 import { getSupabase } from '@/lib/supabaseServer';
 import { NextResponse } from 'next/server';
 
+export async function OPTIONS(request: Request) {
+  return handleCors(request)!; // handles preflight
+}
+
 export async function PUT(req: Request) {
+  const cors = handleCors(req);
+
   try {
     const body = await req.json();
     const { user_id, clock_out_photo } = body;
 
     if (!user_id) {
-      return NextResponse.json({ error: 'Missing fields' }, { status: 400 });
+      return NextResponse.json(
+        { error: 'Missing fields' },
+        { status: 400, headers: cors?.headers },
+      );
     }
 
     // Insert into 'users' table
@@ -20,15 +30,24 @@ export async function PUT(req: Request) {
       .limit(1);
 
     if (dbError) {
-      return NextResponse.json({ error: dbError.message }, { status: 500 });
+      return NextResponse.json(
+        { error: dbError.message },
+        { status: 500, headers: cors?.headers },
+      );
     }
 
-    return NextResponse.json({
-      message: 'Clock out successful',
-      user: data?.[0],
-    });
+    return NextResponse.json(
+      {
+        message: 'Clock out successful',
+        user: data?.[0],
+      },
+      { headers: cors?.headers },
+    );
   } catch (err) {
     console.error(err);
-    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Server error' },
+      { status: 500, headers: cors?.headers },
+    );
   }
 }
